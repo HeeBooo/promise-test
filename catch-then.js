6deec36c875af5b357b629addb56e73cef8b578d
HeeBooo <1807941596@qq.com>
2018-03-08 11:34:11 +0800

    错误处理完结

---
+++ catch-then.js
@@ -0,0 +1,24 @@
+console.log('start');
+
+new Promise(resolve => {
+    setTimeout(() => {
+        resolve();
+    }, 1000)
+}).then(() => {
+    console.log('satrt');
+    throw new Error('test error');
+}).catch(err => {
+    console.log('i catch:', err);
+    // 情况二
+    throw new  Error('another error')
+}).then(() => {
+    console.log('arrive here');
+}).then(() => {
+    console.log('and here');
+}).catch(err => {
+    console.log('now,i catch:', err);
+})
+
+
+// 第一个catch执行后，后面的then也执行了，但是后面的catch就不会执行了
+// 情况二 ，第一个catch后面的then不执行，直接执行catch
--- error-reject.js
+++ error-reject.js
@@ -3,9 +3,11 @@ console.log('start');
 new Promise((resolve, reject) => {
     setTimeout(() => {
         reject('bye')
-    });
+    }, 2000);
 }).then(value => {
     console.log(value + 'world')
 }, value => {
     console.log('Error:', value);
 });
+
+// 推荐使用第一种方式error.js，清晰好读，并可以捕获前面then的错误
